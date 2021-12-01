f51028d2fd0c4f3d84c9c4736fbd4c933a0facc3
Raffrerty <raffsimms23@gmail.com>
2021-12-01 16:42:13 +0000

    Update Test Script

--- firstyear/Multiplayer-Quiz_Program/run_test_setup.bat
+++ firstyear/Multiplayer-Quiz_Program/run_test_setup.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-start "SERVER" python server/main.py & timeout 0 & start client/env/Scripts/python client/main.py & timeout 0 & start client/env/Scripts/python client/main.py
+start "SERVER" client/env/Scripts/python server/main.py & timeout 0 & start client/env/Scripts/python client/main.py & timeout 0 & start client/env/Scripts/python client/main.py
 
 

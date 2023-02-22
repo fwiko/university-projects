20f36f9245772f36a84e82383e02ba4199c986de
fwiko <raff@raffsimms.com>
2023-02-22 19:29:31 +0000

    Load-Balancer: Message structure experimenting

--- 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/messages/Message.java
+++ 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/messages/Message.java
@@ -4,10 +4,19 @@
  */
 package loadbalancer.messages;
 
+import java.util.Arrays;
+
 /**
  *
  * @author fwiko
  */
 public class Message {
+    private String messageInstruction = null;
+    private String[] messageParameters = null;
     
+    public Message(String messageString, String separator) {
+        String[] params = messageString.split(separator);
+        messageInstruction = params[0];
+        messageParameters = Arrays.copyOfRange(params, 1, params.length);
+    }
 }

6df4ae8971083e45304d020d3fc1bff1949ba23b
fwiko <raff@raffsimms.com>
2023-02-22 13:59:39 +0000

    Load-Balancer: Added Load-Balancer Classes

--- 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/LoadBalancer.java
+++ 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/LoadBalancer.java
@@ -3,6 +3,13 @@ package loadbalancer;
 public class LoadBalancer {
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java loadbalancer <port>");
+            System.exit(0);
+        }
+        
+        int port = Integer.parseInt(args[0]);
+        
         
     }
     

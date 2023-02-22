abb0ca4cadacc2ef2493c622d989cd42f13ce401
fwiko <raff@raffsimms.com>
2023-02-22 14:43:47 +0000

    Load-Balancer: Port input for server listener

--- 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/LoadBalancer.java
+++ 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/LoadBalancer.java
@@ -3,13 +3,31 @@ package loadbalancer;
 public class LoadBalancer {
 
     public static void main(String[] args) {
+        // Attempt to retrieve the first input argument (port number)
         if (args.length < 1) {
             System.out.println("Usage: java loadbalancer <port>");
             System.exit(0);
         }
         
-        int port = Integer.parseInt(args[0]);
+        // Process the input port number that Server will listen for Messages on
+        int port = 0;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Input \"" + args[0] + "\" is not a valid Integer.");
+            System.exit(0);
+        }
         
+        // Initialise a new Server/Listener instance
+        Server listenerServer = new Server(port);
+        
+        // Start the Server to listen for and handle Messages from Initiator and Node(s)
+        try {
+            listenerServer.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
         
     }
     
--- 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/Server.java
+++ 202223/Term2/LoadBalancer/LoadBalancer/src/loadbalancer/Server.java
@@ -9,5 +9,16 @@ package loadbalancer;
  * @author fwiko
  */
 public class Server {
+    private int port = 0;
     
+    public Server(int port) {
+        this.port = port;
+    }
+    
+    public void start() {
+        if (port < 1 || port >= 65535) {
+            throw new IllegalArgumentException("Error: Port \"" + port + "\" is outside of the valid range.");
+        }
+        System.out.println("Done");
+    }
 }
